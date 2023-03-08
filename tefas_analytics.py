37bdce73ba4f20b9c673997cd41f52883eddb6a5
Smark2019 <salih8461921@outlook.com>
2023-03-08 18:20:57 +0300

    data fetching and parsing tuned for all ranges

--- tefas_analytics.py
+++ tefas_analytics.py
@@ -1,16 +1,49 @@
 from tefas import Crawler
+from dateutil.relativedelta import relativedelta
+from datetime import datetime,timedelta
+import pandas as pd
+
 
 tefas = Crawler()
 
-data = tefas.fetch(start="2022-09-01", end="2022-12-01", name="OSD", columns=["code", "date", "price","title"])
-print(data.describe())
+def print_df(data):
+    for index, row in data.iterrows():
+
+  
+        print(row['date'], row['price'])
+
+def date_parser(start_date, end_date):
+    delta = relativedelta(months=3)
+    price_data = pd.DataFrame()
+    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date, "%Y-%m-%d")
+    while start_date < end_date:
+        next_date = start_date + delta
+        if next_date > end_date:
+            next_date = end_date
+        start_str = start_date.strftime("%Y-%m-%d")
+        end_str = next_date.strftime("%Y-%m-%d")
+        data = tefas.fetch(start=start_str, end=end_str, name="OSD", columns=["code", "date", "price", "title"])
+        price_data = pd.concat([price_data, data])
+        #print_df(price_data)
+        #print("- - - - - - -")
+        start_date = next_date + timedelta(days=1)
+        #print(f"start date :{start_date}")
+    return price_data.sort_values(by='date',ascending=True)
 
 
-"""
+
+
+
+
+data = date_parser(start_date="2022-11-15", end_date="2023-03-05")
+print_df(data)
+    
+    
+
 import matplotlib.pyplot as plt
 #x_axis = list(range(len(balance_list_sc)))
-plt.plot(data[("date")], data[("price")], label="Prices")
+plt.plot(data[("date")], data[("price")])
 plt.title("OSD Prices")
 plt.show()
 
-"""
