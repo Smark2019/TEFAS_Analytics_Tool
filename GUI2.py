60d268eb9a9de3db5e8530c38897ef23d2ca800b
Smark2019 <salih8461921@outlook.com>
2023-03-09 18:03:19 +0300

    made more function based

--- GUI2.py
+++ GUI2.py
@@ -5,6 +5,8 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHB
 from PyQt5.QtCore import QDate, Qt
 import numpy as np
 
+from tefas_analytics import date_parser
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
--- tefas_analytics.py
+++ tefas_analytics.py
@@ -3,16 +3,17 @@ from dateutil.relativedelta import relativedelta
 from datetime import datetime,timedelta
 import pandas as pd
 from tabulate import tabulate
+import matplotlib.pyplot as plt
 
-tefas = Crawler()
+
 
 def print_df(data):
-    for index, row in data.iterrows():
+    print(tabulate(data.reset_index(drop=True), headers = 'keys', tablefmt = 'pretty'))
 
-  
-        print(row['date'], row['price'])
 
 def date_parser(start_date, end_date, fund_code):
+    tefas = Crawler()
+
     delta = relativedelta(months=3)
     price_data = pd.DataFrame()
     start_date = datetime.strptime(start_date, "%Y-%m-%d")
@@ -27,11 +28,28 @@ def date_parser(start_date, end_date, fund_code):
         price_data = pd.concat([price_data, data])
         start_date = next_date + timedelta(days=1)
 
-    return price_data.sort_values(by='date',ascending=True)
+    return price_data.sort_values(by='date',ascending=True) # returns df
+
+
+def get_CAGR(price_data_df):
+    # Calculate the beginning and ending values of the fund's price data
+    beginning_value = price_data_df.iloc[0]["price"]
+    ending_value = price_data_df.iloc[-1]["price"]
+
+    # Calculate the number of years between the beginning and ending dates of the fund's price data
+    num_years = (price_data_df.iloc[-1]["date"] - price_data_df.iloc[0]["date"]).days / 365.25
+
+    # Calculate the CAGR of the fund's price data
+    cagr = (ending_value / beginning_value) ** (1 / num_years) - 1
+
+    return ("CAGR: {:.2%}".format(cagr))        # returns string
 
 
 
-
+def plot_graph(price_data_df):
+    plt.plot(price_data_df[("date")], price_data_df[("price")])
+    plt.title("RPD Prices")
+    plt.show()
 
 if __name__ == "__main__":
 
@@ -40,30 +58,12 @@ if __name__ == "__main__":
     # Retrieve the price data
     data = date_parser(start_date="2019-01-01", end_date="2023-03-01", fund_code="OSD")
 
-    # Convert the date strings to datetime objects
-    #data["date"] = data["date"].apply(parse)
-
-    # Calculate the beginning and ending values of the fund's price data
-    beginning_value = data.iloc[0]["price"]
-    ending_value = data.iloc[-1]["price"]
-
-    # Calculate the number of years between the beginning and ending dates of the fund's price data
-    num_years = (data.iloc[-1]["date"] - data.iloc[0]["date"]).days / 365.25
-
-    # Calculate the CAGR of the fund's price data
-    cagr = (ending_value / beginning_value) ** (1 / num_years) - 1
-
-    print("CAGR: {:.2%}".format(cagr))
-
-        
+    print(get_CAGR(data))
+    plot_graph(data)
         
 
-    import matplotlib.pyplot as plt
-    #x_axis = list(range(len(balance_list_sc)))
-    plt.plot(data[("date")], data[("price")])
-    plt.title("RPD Prices")
-    plt.show()
 
-    print(tabulate(data.reset_index(drop=True), headers = 'keys', tablefmt = 'pretty'))
+    
+
 
     
